694c37b1d9bbf1497ceac378f18226d15055ac26
Poseidonf15 <58133253+poseidonf15@users.noreply.github.com>
2023-09-07 17:04:15 +0300

    First Update
    I added a second play space which contains numbers that can be devided by 0.05.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const questionText = document.querySelector("#questionText");
 const playerAnswer = document.querySelector("#playerAnswer");
 const submitBtn = document.querySelector("#submitBtn");
-const clearBtn = document.querySelector("#clearBtn");
 let answer = 4;
 let num1;
 let num2;
 let symbol;
 
 submitBtn.addEventListener("click", submit);
-clearBtn.addEventListener("click", clear);
+playerAnswer.addEventListener("click", clear);
 
 function submit(){
     if(playerAnswer.value == answer){
         symbol = Math.floor(Math.random() * 4) + 1;
-        console.log(symbol)
         
         switch(symbol){
             case(1):
@@ -47,5 +45,82 @@ function submit(){
 };
 
 function clear(){
-    playerAnswer.value = '';
+    playerAnswer.value = "";
 };
+
+const questionText2 = document.querySelector("#questionText2");
+const playerAnswer2 = document.querySelector("#playerAnswer2");
+const submitBtn2 = document.querySelector("#submitBtn2");
+let answer2 = 1.5;
+let symbols = ["+", "-", "*"];
+let question = [ , , , , ];
+
+submitBtn2.addEventListener("click", submit2);
+playerAnswer2.addEventListener("click", clear2);
+
+function submit2(){
+    if (answer2 == playerAnswer2.value){
+        question[1] = symbols[Math.floor(Math.random() * 3)];
+        question[3] = symbols[Math.floor(Math.random() * 3)];
+
+        for(let i = 0; i < 5; i += 2){
+            question[i] = ((Math.floor(Math.random() * 200) + 1) * 0.05).toFixed(2);
+        };
+
+        question[1] = symbols[0];
+        question[3] = symbols[2];
+        if (question[1] == "+"){
+            if (question[3] == "+"){
+                answer2 = Number(question[0]) + Number(question[2]) + Number(question[4]);
+            };
+            if (question[3] == "-"){
+                answer2 = Number(question[0]) + Number(question[2]) - Number(question[4]);
+            };
+            if (question[3] == "*"){
+                question[(Math.floor(Math.random() * 2) + 1) * 2] = Math.floor(Math.random() * 20) + 1;
+                answer2 = Number(question[0]) + Number(question[2]) * Number(question[4]);
+            };
+        };
+        if (question[1] == "-"){
+            if (question[3] == "+"){
+                answer2 = Number(question[0]) - Number(question[2]) + Number(question[4]);
+            };
+            if (question[3] == "-"){
+                answer2 = Number(question[0]) - Number(question[2]) - Number(question[4]);
+            };
+            if (question[3] == "*"){
+                question[(Math.floor(Math.random() * 2) + 1) * 2] = Math.floor(Math.random() * 20) + 1;
+                answer2 = Number(question[0]) - Number(question[2]) * Number(question[4]);
+            };
+        };
+        if (question[1] == "*"){
+            if (question[3] == "+"){
+                question[(Math.floor(Math.random() * 2) + 1) * 2] = Math.floor(Math.random() * 20) + 1;
+                answer2 = Number(question[0]) * Number(question[2]) + Number(question[4]);
+            };
+            if (question[3] == "-"){
+                question[(Math.floor(Math.random() * 2) + 1) * 2] = Math.floor(Math.random() * 20) + 1;
+                answer2 = Number(question[0]) * Number(question[2]) - Number(question[4]);
+            };
+            if (question[3] == "*"){
+                question[0] = Math.floor(Math.random() * 20) + 1;
+                question[4] = Math.floor(Math.random() * 20) + 1;
+                answer2 = Number(question[0]) * Number(question[2]) * Number(question[4]);
+            };
+        };
+
+        console.log(...question);
+
+        answer2 = Math.round(answer2 * 100) / 100;
+        console.log(answer2)
+        questionText2.textContent = `${question[0]}${question[1]}${question[2]}${question[3]}${question[4]}=?`
+    }
+    else{
+        playerAnswer2.value = "Wrong answer :("
+    }
+};
+
+function clear2(){
+    playerAnswer2.value = "";
+};
+
